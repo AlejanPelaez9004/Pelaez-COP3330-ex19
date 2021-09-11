1230122a54f35f9e99d8df54fa6e83c12c012c7f
AlejanPelaez9004 <alejanpelaez@knights.ucf.edu>
2021-09-11 09:16:04 -0700

    Completed

--- src/main/java/Pelaez/Alejandro/App.java
+++ src/main/java/Pelaez/Alejandro/App.java
@@ -3,15 +3,49 @@
  *  Copyright 2021 Alejandro Pelaez
  */
 package Pelaez.Alejandro;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
+    static Scanner input = new Scanner(System.in);
+    static boolean valid = false;
+
     public static void main(String[] args)
     {
-        System.out.print("What is your name?");
         Scanner input = new Scanner(System.in);
-        String name = input.next();
-        System.out.print("Hello " + name + ", nice to meet you!");
-        System.out.print("\n");
+        System.out.print("What is your weight in pounds? ");
+        float weight = ValidateInput();
+        System.out.print("What is your height in inches? ");
+        float height = ValidateInput();
+
+        float bmi = (weight / (height * height)) * 703;
+        System.out.println("Your BMI is " + bmi);
+        if (bmi > 18.5f && bmi < 25)
+        {
+            System.out.println("You are in the ideal weight range");
+        }
+        else
+        {
+            System.out.println("You are overweight. You should see your doctor.");
+        }
+    }
+
+    private static float ValidateInput()
+    {
+        float val = 0;
+        while (!valid)
+        {
+            String in = input.next();
+            try
+            {
+                val = Float.parseFloat(in);
+                valid = true;
+            } catch (NumberFormatException ex)
+            {
+                System.out.println("Incorrect input. Try again: ");
+            }
+        }
+        valid = false;
+        return val;
     }
 }
